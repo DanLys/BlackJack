d6b3945ec72318d4c5e0d6c0ae9d1a28536dcc75
DanLys <lyskin-2013@mail.ru>
2020-09-17 19:18:00 +0300

    Initial Commit

---
+++ BlackJack/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BlackJack
+//
+//  Created by Danil Lyskin on 17.09.2020.
+//  Copyright © 2020 Danil Lyskin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
